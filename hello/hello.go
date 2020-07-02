ca069230ff51d8fbd73c17c498041631169b0bdb
Alex Lewis <alexmichael.lewis@gmail.com>
2020-07-02 02:50:46 -0400

    test(Hello): refactor tests to be more DRY

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 // Hello returns our desired message
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	actual := Hello("world")
-	expected := "Hello, world"
 
-	if actual != expected {
-		t.Errorf("actual %q expected %q", actual, expected)
+	assertCorrectMessage := func(t *testing.T, actual, expected string) {
+		t.Helper()
+		if actual != expected {
+			t.Errorf("actual %q expected %q", actual, expected)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		actual := Hello("Chris")
+		expected := "Hello, Chris"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
+
 }
